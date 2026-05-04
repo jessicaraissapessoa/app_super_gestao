213ad1e5c321f501e5428d8a1bfe54752d78968a
jessicaraissapessoa <jessicaraissapessoa@gmail.com>
2026-05-04 20:34:18 -0300

    Redirect do objeto route

--- routes/web.php
+++ routes/web.php
@@ -29,6 +29,10 @@ Route::prefix('/app')->group(function() {
     Route::get('/produtos', function() { return 'Produtos';})->name('app.produtos');
 });
 
+Route::get('/rota1', function() { echo 'Rota 1'; })->name('site.rota1');
+// Route::get('/rota2', function() { echo 'Rota 2'; })->name('site.rota2');
+Route::redirect('/rota2', '/rota1'); //redireciona a rota 2 para a rota 1
+
 // Route::get(
 //     '/contato/{nome}/{categoria_id}',
 //     function(string $nome, int $categoria_id = 1) {

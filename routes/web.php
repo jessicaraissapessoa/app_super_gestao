c4c05f3531422351e57016830643152d3c130068
jessicaraissapessoa <jessicaraissapessoa@gmail.com>
2026-05-04 19:39:59 -0300

    Tratando parâmetros de rota com regex

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,7 @@ Route::get('/sobre-nos', 'SobreNosController@sobreNos');
 Route::get('/contato', 'ContatoController@contato');
 
 Route::get(
-    '/contato/{nome}/{categoria?}/{assunto?}/{mensagem?}',
-    function(string $nome, string $categoria = 'N/A', string $assunto = 'N/A 2', string $mensagem = 'N/A 3') {
-        echo "Estamos aqui, $nome - $assunto - $categoria - $mensagem";
-});
+    '/contato/{nome}/{categoria_id}',
+    function(string $nome, int $categoria_id = 1) {
+        echo "Estamos aqui, $nome - $categoria_id";
+})->where('categoria_id', '[0-9]+')->where('nome', '[A-Za-z]+');

b3691ca93ded39810d9a1bc6c646a9f375b3441f
jessicaraissapessoa <jessicaraissapessoa@gmail.com>
2026-05-04 19:49:12 -0300

    Implementando versão rudimentar de menu de navegação

--- routes/web.php
+++ routes/web.php
@@ -23,8 +23,8 @@ Route::get('/sobre-nos', 'SobreNosController@sobreNos');
 
 Route::get('/contato', 'ContatoController@contato');
 
-Route::get(
-    '/contato/{nome}/{categoria_id}',
-    function(string $nome, int $categoria_id = 1) {
-        echo "Estamos aqui, $nome - $categoria_id";
-})->where('categoria_id', '[0-9]+')->where('nome', '[A-Za-z]+');
+// Route::get(
+//     '/contato/{nome}/{categoria_id}',
+//     function(string $nome, int $categoria_id = 1) {
+//         echo "Estamos aqui, $nome - $categoria_id";
+// })->where('categoria_id', '[0-9]+')->where('nome', '[A-Za-z]+');

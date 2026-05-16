e0b2dd8bec069d060c9c5a63982ad5728c29373d
jessicaraissapessoa <jessicaraissapessoa@gmail.com>
2026-05-16 16:10:12 -0300

    utilizando @component com parâmetro

--- resources/views/site/contato.blade.php
+++ resources/views/site/contato.blade.php
@@ -8,28 +8,13 @@
         <div class="titulo-pagina">
             <h1>Entre em contato conosco</h1>
         </div>
-
+            @component('site.layouts._components.form_contato', ['classe' => 'borda-preta'])
+                <p> A nossa equipe analisará a sua mensagem e retornaremos o mais brevemente possível.</p>
+                <p>Nosso tempo médio de resposta é de 48 horas.</p>  
+            @endcomponent
         <div class="informacao-pagina">
             <div class="contato-principal">
-                <form action={{ route('site.contato') }} method="post">
-                    @csrf
-                    <input name="nome" type="text" placeholder="Nome" class="borda-preta">
-                    <br>
-                    <input name="telefone" type="text" placeholder="Telefone" class="borda-preta">
-                    <br>
-                    <input name="email" type="text" placeholder="E-mail" class="borda-preta">
-                    <br>
-                    <select name="motivo_contato" class="borda-preta">
-                        <option value="">Qual o motivo do contato?</option>
-                        <option value="1">Dúvida</option>
-                        <option value="2">Elogio</option>
-                        <option value="3">Reclamação</option>
-                    </select>
-                    <br>
-                    <textarea name="mensagem" class="borda-preta">Preencha aqui a sua mensagem</textarea>
-                    <br>
-                    <button type="submit" class="borda-preta">ENVIAR</button>
-                </form>
+                
             </div>
         </div>  
     </div>

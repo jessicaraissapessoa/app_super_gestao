d760745a6d9d326cebc720385c88e15e60cd8442
jessicaraissapessoa <jessicaraissapessoa@gmail.com>
2026-05-17 17:26:29 -0300

    Inserindo registros no banco com create e fillable

--- app/Fornecedor.php
+++ app/Fornecedor.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Fornecedor extends Model
 {
-    //
+    //Corrigindo o nome da tabela no Model para o correto ORM do Eloquent
     protected $table = 'fornecedores';
+
+    //Habilitando o preenchimento através do método create de determinados atributos do objeto:
+    protected $fillable = ['nome','site','uf','email'];
 }

87360e51e9141ab722c583fc08379b4513eafeee
jessicaraissapessoa <jessicaraissapessoa@gmail.com>
2026-05-17 22:12:55 -0300

    Implementando o soft delete em fornecedores

--- app/Fornecedor.php
+++ app/Fornecedor.php
@@ -3,12 +3,16 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Fornecedor extends Model
 {
+    //Implementando soft delete;
+    use SoftDeletes;
+
     //Corrigindo o nome da tabela no Model para o correto ORM do Eloquent
     protected $table = 'fornecedores';
 
-    //Habilitando o preenchimento através do método create de determinados atributos do objeto:
+    //Habilitando o preenchimento através do método create de determinados atributos do objeto - Quais valores podem receber atribuição
     protected $fillable = ['nome','site','uf','email'];
 }
